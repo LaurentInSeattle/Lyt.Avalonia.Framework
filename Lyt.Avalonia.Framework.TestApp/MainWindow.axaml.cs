918b4e3b4633a9dfe7e62dafb1774448566ab68b
Laurent <ly.testud@outlook.com>
2024-09-28 11:04:46 -0700

    Initial check-in: Empty Solution with empty test app

---
+++ Lyt.Avalonia.Framework.TestApp/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Lyt.Avalonia.Framework.TestApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
