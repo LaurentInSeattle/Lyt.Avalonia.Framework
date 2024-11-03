340f629533edd936a8c4bef3cbe404b6bbd36372
Laurent <ly.testud@outlook.com>
2024-11-02 17:41:29 -0700

    Gate phase and rotation editing is complete but serialization is broken : Angle/Axis not saved / restored.

--- Lyt.Avalonia.Persistence/FileManagerModel.cs
+++ Lyt.Avalonia.Persistence/FileManagerModel.cs
@@ -37,7 +37,21 @@ public sealed class FileManagerModel : ModelBase, IModel
     public FileManagerModel(IMessenger messenger, ILogger logger) : base(messenger, logger)
     {
         this.Configuration = new FileManagerConfiguration(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
-        this.jsonSerializerOptions = new JsonSerializerOptions { AllowTrailingCommas = true };
+        this.jsonSerializerOptions = 
+            new JsonSerializerOptions 
+            { 
+                AllowTrailingCommas = true,
+                WriteIndented = true,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                IgnoreReadOnlyFields = true,
+                IgnoreReadOnlyProperties = true,
+                // .Net 9 properties 
+                //
+                // AllowOutOfOrderMetadataProperties = true,
+                // IndentSize = 4,
+                // RespectRequiredConstructorParameters = true,
+                // RespectNullableAnnotations= true,
+            };
         this.jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     }
 
