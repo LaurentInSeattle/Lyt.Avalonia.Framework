3d70eb230c3fed0e7a356f91b3585861a7ec6cc1
Laurent <ly.testud@outlook.com>
2024-11-06 13:02:43 -0800

    Framework: rounded image, finalized use of qubit lists in Stage Operator construction

---
+++ Lyt.Avalonia.Controls/Images/RoundedImage.cs
@@ -0,0 +1,32 @@
+﻿namespace Lyt.Avalonia.Controls.Images;
+
+public class RoundedImage : Image
+{
+    public static readonly AttachedProperty<double> CornerRadiusProperty =
+        AvaloniaProperty.RegisterAttached<RoundedImage, double>(
+            "CornerRadius", typeof(RoundedImage), 8);
+
+    public static void SetCornerRadius(AvaloniaObject element, double parameter)
+        => element.SetValue(CornerRadiusProperty, parameter);
+
+    public static double GetCornerRadius(AvaloniaObject element)
+        => element.GetValue(CornerRadiusProperty);
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        IImage? source = this.Source;
+        if (source is not null)
+        {
+            Size result = 
+                this.Stretch.CalculateSize(availableSize, source.Size, this.StretchDirection);
+            this.Clip =
+                new RectangleGeometry(
+                    new Rect(0, 0, result.Width, result.Height),
+                    RoundedImage.GetCornerRadius(this),
+                    RoundedImage.GetCornerRadius(this));
+            return result;
+        }
+
+        return base.MeasureOverride(availableSize);
+    }
+}
