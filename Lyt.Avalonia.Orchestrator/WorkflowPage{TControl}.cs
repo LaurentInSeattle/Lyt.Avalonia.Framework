18dc86ed76f2469d70680930f871f68f78078df7
Laurent <ly.testud@outlook.com>
2024-11-14 13:25:55 -0800

    WIP: First pass at introducing Workflow manager and state machine

---
+++ Lyt.Avalonia.Orchestrator/WorkflowPage{TControl}.cs
@@ -0,0 +1,10 @@
+﻿namespace Lyt.Avalonia.Orchestrator;
+
+public class WorkflowPage<TState, TTrigger, TControl> : WorkflowPage<TState, TTrigger>
+    where TState : struct, Enum
+    where TTrigger : struct, Enum
+    where TControl : Control, new()
+{
+    public TControl View
+        => this.Control as TControl ?? throw new InvalidOperationException("View is null");
+}
