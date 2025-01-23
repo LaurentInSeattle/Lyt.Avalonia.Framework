c90aa23d8cbb911783ef70f2af25c7eba042b833
Laurent <ly.testud@outlook.com>
2025-01-23 11:41:55 -0800

    Some ideas for Form Validation

---
+++ Lyt.Validation/FieldValidator{T}.cs
@@ -0,0 +1,97 @@
+﻿namespace Lyt.Validation;
+
+public sealed class FieldValidator<T>(
+    Bindable viewModel, FieldValidatorParameters<T> parameters)
+    : FieldValidator(viewModel)
+    where T : IParsable<T>
+{
+    private readonly FieldValidatorParameters<T> parameters = parameters;
+
+    public Type TargetType => typeof(T);
+
+    public FieldValidatorResults<T> Validate()
+    {
+        string ShowValidationMessage(string message)
+        {
+            if (!string.IsNullOrWhiteSpace(this.parameters.MessagePropertyName) &&
+                !string.IsNullOrWhiteSpace(message))
+            {
+                // Localize message if a localizer is available 
+                var localizer = Validators.Validators.Localizer;
+                if (localizer is not null)
+                {
+                    message = localizer.Lookup(message);
+                }
+
+                this.viewModel.Set<string>(this.parameters.SourcePropertyName, message);
+            }
+
+            return message;
+        }
+
+        // Get property value 
+        string propertyText = string.Empty;
+        string? maybePropertyText = this.viewModel.Get<string>(this.parameters.SourcePropertyName);
+        bool isEmpty = string.IsNullOrWhiteSpace(maybePropertyText);
+        if (!isEmpty)
+        {
+            // Trim 
+            propertyText = maybePropertyText!;
+            propertyText = propertyText.Trim();
+            isEmpty = string.IsNullOrWhiteSpace(propertyText);
+        }
+
+        if (isEmpty)
+        {
+            // Clear white space noise 
+            this.viewModel.Set<string>(this.parameters.SourcePropertyName, string.Empty);
+
+            if (this.parameters.AllowEmpty)
+            {
+                return
+                    new FieldValidatorResults<T>(IsValid: true, HasValue: false, Value: default);
+            }
+            else
+            {
+                string emptyMessage = this.parameters.EmptyFieldMessage;
+                emptyMessage = string.IsNullOrWhiteSpace(emptyMessage) ? DefaultEmptyFieldMessage : emptyMessage;
+                emptyMessage = ShowValidationMessage(emptyMessage);
+                return
+                    new FieldValidatorResults<T>(IsValid: false, HasValue: false, Message: emptyMessage);
+            }
+        }
+
+        // Check if parsing is needed 
+        T propertyValue;
+        if (typeof(string).Is<T>() && propertyText is T propertyString)
+        {
+            // Target type is string, no parsing needed
+            propertyValue = propertyString;
+        }
+        else
+        {
+            // Need to parse  
+            bool isParsed = propertyText.TryParse<T>(out T? maybeValue);
+            if (!isParsed || maybeValue is not T value)
+            {
+                // failed to parse
+                string parseMessage = this.parameters.FailedToParseMessage;
+                parseMessage = string.IsNullOrWhiteSpace(parseMessage) ? DefaultFailedToParseMessage : parseMessage;
+                parseMessage = ShowValidationMessage(parseMessage);
+                var parseResults = new FieldValidatorResults<T>(IsValid: false, HasValue: false, Message: parseMessage);
+                return parseResults;
+            }
+
+            propertyValue = value;
+        }
+
+        // Now we have a value: Run the Fluent Validator 
+        bool isValid = propertyValue.IsValid<AbstractValidator<T>, T>(out string message);
+        if (!isValid)
+        {
+            ShowValidationMessage(message);
+        }
+
+        return new FieldValidatorResults<T>(IsValid: isValid, HasValue: true, Value: propertyValue);
+    }
+}
--- Lyt.Validation/FormValidator.cs
+++ Lyt.Validation/FormValidator.cs
@@ -1,14 +1,38 @@
 ﻿namespace Lyt.Validation;
 
-// TODO 
-//
-//public sealed class FormValidator
-//{
-//    private readonly Bindable viewModel;
+public sealed record class FormValidatorResults<T>
+    (
+        T? Value = default,
+        bool IsValid = false,
+        bool HasValue = false,
+        string Message = ""
+    );
 
-//    public FormValidator(Bindable viewModel) 
-//        // TODO , List<FieldValidator> fieldValidators)
-//    {
-//        this.viewModel = viewModel;
-//    }
-//}
+public sealed record class FormValidatorParameters<T>
+    (
+        AbstractValidator<T> Validator,
+        IEnumerable<FieldValidator> FieldValidators, 
+        string MessagePropertyName = ""
+    );
+
+public sealed class FormValidator<T>
+    where T : class
+{
+    private readonly Bindable viewModel;
+    private readonly FormValidatorParameters<T> parameters; 
+    private readonly List<FieldValidator> fieldValidators;
+
+    public FormValidator(Bindable viewModel, FormValidatorParameters<T> parameters)
+    {
+        this.viewModel = viewModel;
+        this.parameters = parameters;
+        this.fieldValidators = new List<FieldValidator>(parameters.FieldValidators);
+    }
+
+    public Type TargetType => typeof(T);
+
+    public FieldValidatorResults<T> Validate()
+    {
+        return new FieldValidatorResults<T>();
+    }
+}
