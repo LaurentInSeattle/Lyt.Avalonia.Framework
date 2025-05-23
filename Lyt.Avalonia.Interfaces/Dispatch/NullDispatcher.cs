fcccd27b371e9a4a58838be8aa654bae8554b31e
Laurent <ly.testud@outlook.com>
2025-05-23 10:11:54 -0700

    Utility assembly with no Avalonia dependency

--- Lyt.Avalonia.Interfaces/Dispatch/NullDispatcher.cs
+++ Lyt.Avalonia.Interfaces/Dispatch/NullDispatcher.cs
@@ -2,7 +2,7 @@
 
 public class NullDispatcher : IDispatch
 {
-    public void OnUiThread(Action action) { }
+    public void OnUiThread(Action action) => action();
 
-    public void OnUiThread<TArgs>(Action<TArgs> action, TArgs args) { }
+    public void OnUiThread<TArgs>(Action<TArgs> action, TArgs args) => action(args);
 }
